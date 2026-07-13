33b92da627a50e063e44cf13c876f2af49cb86b9
Yossef-Elbrmbaly <149072029+Yossef-Elbrmbaly@users.noreply.github.com>
2026-07-14 02:34:35 +0300

    Update database connection details

--- config/Database.php
+++ config/Database.php
@@ -6,10 +6,13 @@ use PDO;
 use PDOException;
 
 class Database {
-    private string $host = "localhost";
-    private string $db_name = "student_management";
-    private string $username = "root";
-    private string $password = "";
+    private string $host = "db.fr-pari1.bengt.wasmernet.com";
+    private string $port = "
+10272";
+    private string $db_name = "DB_NAME";
+    private string $username = "user_99fbe122";
+    private string $password = "pw_FeJluL9CUR95XiYE6ODwRAUfUJZIE4cS
+";
     public ?PDO $conn = null;
 
     public function connect(): ?PDO {
@@ -18,7 +21,8 @@ class Database {
                 $this->conn = new PDO(
                     "mysql:host=" . $this->host . ";dbname=" . $this->db_name, 
                     $this->username, 
-                    $this->password
+                    $this->password,
+                    $this->port
                 );
                 
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
