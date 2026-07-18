e222d2fd675ee58554bde8704b4a5c4b341579ec
Yossef-Elbrmbaly <149072029+Yossef-Elbrmbaly@users.noreply.github.com>
2026-07-19 00:50:01 +0300

    check2

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ error_reporting(E_ALL);
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+$dotenv->safeLoad();
 
 use App\Config\Database;
 use App\Controllers\CourseController;
